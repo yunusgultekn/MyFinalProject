9cf943ea5eade9aaa711b0343e51d4285085e8a6
Yunus3438 <myunus.3438@gmail.com>
2021-03-18 22:21:12 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Business.Concrete;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,71 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> { 
+                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitsInStock=15,UnitPrice=15},
+                new Product{ProductId=2,CategoryId=2,ProductName="Kamera",UnitsInStock=3,UnitPrice=500},
+                new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitsInStock=2,UnitPrice=1500},
+                new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitsInStock=65,UnitPrice=150},
+                new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitsInStock=1,UnitPrice=85}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            
+        }
+    }
+}
